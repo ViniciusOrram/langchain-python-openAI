d7e7293d4c24ed16c5d89c655492c065ecc140bb
Vinicius Costa <vinicius.costa@orram.local>
2025-12-02 15:34:05 -0300

    feat: Defining the model response as a JSON object

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 
 import os
+
+##set_debug(True)
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("motivo pela qual é interessante visitar essa cidade")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 prompt_cidade = PromptTemplate(
     template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
-    """
-    input_variables=["interesse"]
+    {formato_de_saida}
+    """,
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
+
 modelo = ChatOpenAI(
     model='gpt-5-nano',
     temperature=0.5,
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia = prompt_cidade | modelo | parseador
 
 reposta = cadeia.invoke(
     {

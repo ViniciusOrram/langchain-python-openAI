e2b975f67a180fbfcb78ffe43a999d91dde38b21
Vinicius Costa <vinicius.costa@orram.local>
2025-12-04 12:09:43 -0300

    feat: Adding memory to chat with LangChain

--- main_chat.py
+++ main_chat.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 load_dotenv()
 api_key = os.get("OPEN_AI_KEY")
@@ -11,13 +15,43 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
+prompt_sugestoes = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Você é um guia de viagem especializado em destinos brasileiros. Apresente-se como Sr. Passeios."),
+        ("placeholder", "{historico}"),
+        ("human", "{query}")
+    ]
+)
+
+cadeia = prompt_sugestoes | modelo | StrOutputParser()
+
+memoria = {}
+sessao = "langchain_1"
+
+def historico_por_sessao(sessao : str):
+    if sessao not in memoria:
+        memoria[sessao] = InMemoryChatMessageHistory()
+    return memoria[sessao]
+
 lista_perguntas = [
     "Quero visitar um lugar no Brasil, famoso por praias e cultura. Pode me sugerir?",
     "Qual a melhor época do ano para ir?"
 ]
 
+cadeia_com_memoria = RunnableWithMessageHistory(
+    runnable=cadeia,
+    get_session_history=historico_por_sessao,
+    input_messages_key="query",
+    history_messages_key="historico"
+)
+
 for uma_pergunta in lista_perguntas:
-    resposta = modelo.invoke(uma_pergunta)
+    resposta = cadeia_com_memoria.invoke(
+        {
+            "query" : uma_pergunta
+        },
+        config={"session_id" : sessao}
+    )
     print("Usuário: ", uma_pergunta),
-    print("AI: ", resposta.content, "\n")
+    print("AI: ", resposta, "\n")
 
